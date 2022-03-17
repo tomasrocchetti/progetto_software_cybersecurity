a0f49c36c3614d8d0aee3b73d9be7324d35b0162
tomasrocchetti <40356707+tomasrocchetti@users.noreply.github.com>
2022-03-17 18:43:23 +0100

    scheletro funzionante per la gestione dei token
    Al momento è solo in grado di generare un NFT

---
+++ tokenHandler.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.2;
+
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+ 
+
+contract mNFT is NFTokenMetadata, Ownable {
+ 
+  constructor() {
+    nftName = "Carbon Footprint";
+    nftSymbol = "CBF";
+  }
+ 
+  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+    super._mint(_to, _tokenId);
+    super._setTokenUri(_tokenId, _uri);
+  }
+ 
+}
