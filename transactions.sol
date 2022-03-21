f8880d5b6f0b18893e706caa70c294ee9efc72df
tomasrocchetti <40356707+tomasrocchetti@users.noreply.github.com>
2022-03-21 09:46:25 +0100

    separate funzioni di aggiunta materia prima e prodotto lavorato

--- transactions.sol
+++ transactions.sol
@@ -13,7 +13,7 @@ struct Product {
     string name;    // nome del prodotto
     string ID;  // identificativo univoco del prodotto (anche numero di lotto)
     uint gCO2;  // grammi di CO2 usati per produrre o per fare la trasformazione
-    uint isProcessed;   // 0 se è una materia prima, 1 se è un derivato
+    bool isProcessed;   // 0 se è una materia prima, 1 se è un derivato
     string productsUsedToProcessIDs;    // se è un derivato elencare tutti gli ID delle materie prime usate, deve essere un array
     uint quantityForUsedProducts;   // per ogni ID di materia prima indicare la quantità utilizzata, anche questo deve essere un array
     uint quantity;  //quantità di prodotto, utile anche nel caso in cui per un prodotto finale venga usata solo una parte di un lotto di materia prima
@@ -25,15 +25,34 @@ mapping (string => Product) Products;
 string[] public productIDs;
 
 // funzione per aggiungere una nuova transazione (ovvero una nuova materia prima o un nuovo prodotto frutto di lavorazione)
-function addProduct(string memory _name, string calldata _ID,  uint _gCO2, uint _isProcessed, string memory _productsUsedToProcessID, uint _quantityForUsedProducts, uint _quantity) public {
+function addMateriaPrima(string memory _name, string calldata _ID,  uint _gCO2, uint _quantity) public {
        
        // si assicura che siano soltanto produttore e trasformatore ad aggiungere nuovi prodotti o materie prime, il cliente non può
-       if(msg.sender == produttore || msg.sender == trasformatore){
+       if(msg.sender == produttore){
         Product storage prod = Products[_ID];
         prod.name = _name;
         prod.ID = _ID;
         prod.gCO2=_gCO2;
-        prod.isProcessed = _isProcessed;
+        prod.isProcessed = false;
+        prod.quantityForUsedProducts = _quantity;
+
+        productIDs.push(_ID);
+       }
+       else{
+        // errore, non puoi completare l'operazione
+       }
+    }
+
+// funzione per aggiungere una nuova transazione (ovvero una nuova materia prima o un nuovo prodotto frutto di lavorazione)
+function addProdottoTrasformato(string memory _name, string calldata _ID,  uint _gCO2_production, string memory _productsUsedToProcessID, uint _quantityForUsedProducts, uint _quantity) public {
+       
+       // si assicura che siano soltanto produttore e trasformatore ad aggiungere nuovi prodotti o materie prime, il cliente non può
+       if(msg.sender == trasformatore){
+        Product storage prod = Products[_ID];
+        prod.name = _name;
+        prod.ID = _ID;
+        prod.gCO2=_gCO2_production+(getCO2ByID(_ID)*_quantityForUsedProducts);
+        prod.isProcessed = true;
         prod. productsUsedToProcessIDs = _productsUsedToProcessID;
         prod.quantityForUsedProducts = _quantityForUsedProducts;
         prod.quantityForUsedProducts = _quantity;
@@ -46,7 +65,7 @@ function addProduct(string memory _name, string calldata _ID,  uint _gCO2, uint 
     }
     
     
-function getProductByID(string memory _ID) view public returns (string memory, string memory, uint, uint, string memory, uint, uint){
+function getProductByID(string memory _ID) view public returns (string memory, string memory, uint, bool, string memory, uint, uint){
     return (Products[_ID].name,
     Products[_ID].ID,
     Products[_ID].gCO2,
