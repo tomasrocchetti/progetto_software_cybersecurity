be016c59cc27a642de3163477c84521fdcf5354d
tomasrocchetti <40356707+tomasrocchetti@users.noreply.github.com>
2022-04-20 21:47:25 +0200

    rimossa quantità per prodotti usati
    è sufficiente inserire più volte lo stesso prodotto

--- src/contracts/Transactions.sol
+++ src/contracts/Transactions.sol
@@ -19,7 +19,6 @@ struct Product {
     uint gCO2;  // grammi di CO2 usati per produrre o per fare la trasformazione
     bool isProcessed;   // 0 se è una materia prima, 1 se è un derivato
     string[] productsUsedToProcessIDs;    // se è un derivato elencare tutti gli ID delle materie prime usate, deve essere un array
-    uint[] quantityForUsedProducts;   // per ogni ID di materia prima indicare la quantità utilizzata, anche questo deve essere un array
     uint quantity;  //quantità di prodotto, utile anche nel caso in cui per un prodotto finale venga usata solo una parte di un lotto di materia prima
     }
 
@@ -29,7 +28,7 @@ mapping (string => Product) Products;
 string[] public productIDs;
 
 event newMateriaPrima (string name, string ID, uint gCO2, uint quantity);
-event newProdottoTrasformato (string name, string ID, uint sommaCO2, string[] productsUsedToProcessID, uint[] quantityForUsedProducts, uint quantity);
+event newProdottoTrasformato (string name, string ID, uint sommaCO2, string[] productsUsedToProcessID, uint quantity);
 
 
  
@@ -55,7 +54,7 @@ function addMateriaPrima(string memory _name, string calldata _ID,  uint _gCO2, 
     }
 
 // funzione per aggiungere una nuova transazione (ovvero una nuova materia prima o un nuovo prodotto frutto di lavorazione)
-function addProdottoTrasformato(string memory _name, string calldata _ID,  uint _gCO2_production, string[] memory _productsUsedToProcessID, uint[] memory _quantityForUsedProducts, uint _quantity) public {
+function addProdottoTrasformato(string memory _name, string calldata _ID,  uint _gCO2_production, string[] memory _productsUsedToProcessID, uint _quantity) public {
        
        // si assicura che sia soltanto il trasformatore ad aggiungere nuovi prodotti 
        // if(msg.sender == trasformatore && !idExist(_ID))
@@ -68,14 +67,13 @@ function addProdottoTrasformato(string memory _name, string calldata _ID,  uint 
             
         uint sommaCO2 = 0;
         for (uint i = 0; i< _productsUsedToProcessID.length; i++){
-            sommaCO2 = sommaCO2 + (getCO2ByID(_productsUsedToProcessID[i])*_quantityForUsedProducts[i]);
+            sommaCO2 = sommaCO2 + (getCO2ByID(_productsUsedToProcessID[i]));
         }
         sommaCO2 = sommaCO2 +_gCO2_production;
         
         prod.gCO2=sommaCO2;
         prod.isProcessed = true;
         prod.productsUsedToProcessIDs = _productsUsedToProcessID;
-        prod.quantityForUsedProducts = _quantityForUsedProducts;
         prod.quantity = _quantity;
 
         productIDs.push(_ID);
@@ -86,8 +84,8 @@ function addProdottoTrasformato(string memory _name, string calldata _ID,  uint 
     }
     
     
-function getProductByID(string memory _ID) view public returns (string memory, string memory, uint, bool, string[] memory, uint[] memory, uint){
-    return (Products[_ID].name, Products[_ID].ID, Products[_ID].gCO2, Products[_ID].isProcessed, Products[_ID].productsUsedToProcessIDs, Products[_ID].quantityForUsedProducts, Products[_ID].quantity);
+function getProductByID(string memory _ID) view public returns (string memory, string memory, uint, bool, string[] memory, uint){
+    return (Products[_ID].name, Products[_ID].ID, Products[_ID].gCO2, Products[_ID].isProcessed, Products[_ID].productsUsedToProcessIDs, Products[_ID].quantity);
     }
     
 function getCO2ByID(string memory _ID) view public returns (uint){
