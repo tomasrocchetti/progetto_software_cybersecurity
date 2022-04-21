980a0dd4014265b1bb38f15c79b917ad94a9e55e
tomasrocchetti <40356707+tomasrocchetti@users.noreply.github.com>
2022-04-21 14:07:38 +0200

    ID string->uint

--- src/contracts/Transactions.sol
+++ src/contracts/Transactions.sol
@@ -15,25 +15,25 @@ address cliente;
 
 struct Product {
     string name;    // nome del prodotto
-    string ID;  // identificativo univoco del prodotto (anche numero di lotto)
+    uint ID;  // identificativo univoco del prodotto (anche numero di lotto)
     uint gCO2;  // grammi di CO2 usati per produrre o per fare la trasformazione
     bool isProcessed;   // 0 se è una materia prima, 1 se è un derivato
-    string[] productsUsedToProcessIDs;    // se è un derivato elencare tutti gli ID delle materie prime usate, deve essere un array
+    uint[] productsUsedToProcessIDs;    // se è un derivato elencare tutti gli ID delle materie prime usate, deve essere un array
     uint quantity;  //quantità di prodotto, utile anche nel caso in cui per un prodotto finale venga usata solo una parte di un lotto di materia prima
     }
 
 // mapping della lista di prodotti
-mapping (string => Product) Products;
+mapping (uint => Product) Products;
 
-string[] public productIDs;
+uint[] public productIDs;
 
-event newMateriaPrima (string name, string ID, uint gCO2, uint quantity);
-event newProdottoTrasformato (string name, string ID, uint sommaCO2, string[] productsUsedToProcessID, uint quantity);
+event newMateriaPrima (string name, uint ID, uint gCO2, uint quantity);
+event newProdottoTrasformato (string name, uint ID, uint sommaCO2, uint[] productsUsedToProcessID, uint quantity);
 
 
  
 // funzione per aggiungere una nuova transazione (ovvero una nuova materia prima o un nuovo prodotto frutto di lavorazione)
-function addMateriaPrima(string memory _name, string calldata _ID,  uint _gCO2, uint _quantity) public {
+function addMateriaPrima(string memory _name, uint _ID,  uint _gCO2, uint _quantity) public {
        
        // si assicura che sia soltanto il produttore ad aggiungere nuovi materie prime
        require(msg.sender == produttore);
@@ -54,7 +54,7 @@ function addMateriaPrima(string memory _name, string calldata _ID,  uint _gCO2, 
     }
 
 // funzione per aggiungere una nuova transazione (ovvero una nuova materia prima o un nuovo prodotto frutto di lavorazione)
-function addProdottoTrasformato(string memory _name, string calldata _ID,  uint _gCO2_production, string[] memory _productsUsedToProcessID, uint _quantity) public {
+function addProdottoTrasformato(string memory _name, uint _ID,  uint _gCO2_production, uint[] memory _productsUsedToProcessID, uint _quantity) public {
        
        // si assicura che sia soltanto il trasformatore ad aggiungere nuovi prodotti 
        // if(msg.sender == trasformatore && !idExist(_ID))
@@ -84,11 +84,11 @@ function addProdottoTrasformato(string memory _name, string calldata _ID,  uint 
     }
     
     
-function getProductByID(string memory _ID) view public returns (string memory, string memory, uint, bool, string[] memory, uint){
+function getProductByID(uint _ID) view public returns (string memory, uint, uint, bool, uint[] memory, uint){
     return (Products[_ID].name, Products[_ID].ID, Products[_ID].gCO2, Products[_ID].isProcessed, Products[_ID].productsUsedToProcessIDs, Products[_ID].quantity);
     }
     
-function getCO2ByID(string memory _ID) view public returns (uint){
+function getCO2ByID(uint _ID) view public returns (uint){
     return Products[_ID].gCO2;
     }
     
@@ -96,18 +96,18 @@ function getLen() view public returns (uint){
     return productIDs.length;
     }
     
-function getNameByID(string memory _ID) view public returns (string memory){
+function getNameByID(uint _ID) view public returns (string memory){
     return Products[_ID].name;
     }
     
-function getQtyByID(string memory _ID) view public returns (uint){
+function getQtyByID(uint _ID) view public returns (uint){
     return Products[_ID].quantity;
     }
     
     
-function idExist(string memory _ID) view public returns (bool){
+function idExist(uint _ID) view public returns (bool){
      bool exist = true;
-     if(keccak256(abi.encodePacked(Products[_ID].ID)) == keccak256(abi.encodePacked(""))){
+     if(Products[_ID].ID == 0){
          exist = false;
      } else{
          exist = true;
