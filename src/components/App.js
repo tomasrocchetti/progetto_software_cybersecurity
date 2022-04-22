2a6cbea0caa5f54fb4cd856c543ea34710957a37
tomasrocchetti <40356707+tomasrocchetti@users.noreply.github.com>
2022-04-22 12:45:20 +0200

    funzione per controllare se una stringa è uint

--- src/components/App.js
+++ src/components/App.js
@@ -121,7 +121,16 @@ class App extends Component {
         }
     }
   
-   
+    isPositiveInteger(str) {
+        if (typeof str !== 'string') {
+            return false;
+            }
+        const num = Number(str);
+        if (Number.isInteger(num) && num > 0) {
+            return true;
+            }
+        return false;
+    }
 
     
 
